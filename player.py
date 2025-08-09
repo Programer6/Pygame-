f6da1a2ac33b9b95bd2137ce9f84f5d8f0800118
Programer6 <delroyprithvi@gmail.com>
2025-08-09 15:58:02 +0200

    typo fix

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 from settings import *
 
-class Payer(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load('../graphic/test/player.png').convert_alpha()
