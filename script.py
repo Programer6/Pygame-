ced7c12ec3522ec689b7b98e190500fd5b3b32a9
Programer6 <delroyprithvi@gmail.com>
2025-08-09 14:48:31 +0200

    username

--- script.py
+++ script.py
@@ -1,23 +1,12 @@
-class Dog: 
-    def __init__(self ,name, breed, Owner):
-        self.name  = name
-        self.breed = breed
-        self.Owner = Owner
-    def bark(self):
-        print("Woof! Woof!")
+class User:
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password  
+        self._email = email
 
-class Owner:
-    def __init__(self, name, address):
-        self.name = name
-        self.address = address
+    def clean_email(self):
+        return self._email.replace(" ", "").lower()
 
+User1 = User("user1", "password1", "duiheuidh")
 
-Owner1 = Owner("jfifjeufh","3083904839048")
-dog1 = Dog( "sigma", "bulldog", Owner1)
-print(dog1.Owner.name)
-
-dog2 = Dog("him", "poodle", Owner1)
-dog2.bark()
-print(f"{dog2.name} is a {dog2.breed}.")
-
-
+print(User1.clean_email())
